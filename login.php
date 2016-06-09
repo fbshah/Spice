bd445c5531205d49a6e0daddbb4a06d1cb7c2eae
Frakhanda Shah <fbshah@hotmail.co.uk>
2016-06-09 20:01:29 +0100

    repo initialised

---
+++ login.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+  	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+  	<link rel="shortcut icon" href="favicon.ico">
+
+  	<!-- Google Webfont -->
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+	<!-- Themify Icons -->
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+	<!-- Easy Responsive Tabs -->
+	<link rel="stylesheet" href="css/easy-responsive-tabs.css">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Theme Style -->
+	<link rel="stylesheet" href="css/style.css">
+
+		    <link href="themes/1/js-image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="themes/1/js-image-slider.js" type="text/javascript"></script>
+    <link href="generic.css" rel="stylesheet" type="text/css" />
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body class="inner-page">
+
+		<!-- START #fh5co-header -->
+		<header id="fh5co-header-section" role="header" class="" >
+			<div class="container">
+
+				
+
+				<!-- <div id="fh5co-menu-logo"> -->
+					<!-- START #fh5co-logo -->
+	<h1 id="fh5co-logo" class="pull-left"><a>Spice</a></h1>
+					
+					<!-- START #fh5co-menu-wrap -->
+					<nav id="fh5co-menu-wrap" role="navigation">
+						
+						
+						<ul class="sf-menu" id="fh5co-primary-menu">
+							<li class="active">
+								<a href="index.html">Home</a>
+							</li>
+							
+							<li><a href="news.html">News</a></li>
+							<li><a href="contact.html">Schedule</a></li>
+							<li><a href="contact.html">Advertise Here</a></li>
+							<li><a href="about.html">About Us</a></li>
+							<li><a href="login.php">login</a></li>
+							<li><a href="login.php">Sign Up</a></li>
+						</ul>
+						
+					</nav>
+				<!-- </div> -->
+
+			</div>
+		</header>
+	
+		<div id="fh5co-hero" style="background-image: url(images/3.jpg);">
+			<a href="#fh5co-main" class="smoothscroll fh5co-arrow to-animate hero-animate-4"><i class="ti-angle-down"></i></a>
+			<!-- End fh5co-arrow -->
+			<div class="container">
+				<div class="col-md-12">
+					<div class="fh5co-hero-wrap">
+						<div class="fh5co-hero-intro">
+														<div id="sliderFrame">
+        <div id="slider">
+            <a href="http://wwwe.spicefm107.com" target="_blank">
+                <img src="images/img_large_5.jpg" alt="Welcome to Spicefm107.com" />
+            </a>
+            <img src="images/img_large_1.jpg" />
+            <img src="images/img_large_2.jpg" alt="" />
+            <img src="images/img_large_3.jpg" alt="#htmlcaption" />
+            <img src="images/img_large_4.jpg" />
+        </div>
+    </div>
+							<h1 class="to-animate hero-animate-1">Contact Us</h1>
+														<p><script>
+  // MixStream Flash Player, http://mixstreamflashplayer.net/
+  var flashvars = {};
+  flashvars.serverHost = "s6.myradiostream.com:12306/;";
+  flashvars.getStats = "1";
+  flashvars.autoStart = "0";
+  var params = {};params.bgcolor= "";
+  params.wmode="transparent";
+</script>
+<script type="text/javascript" src="http://mixstreamflashplayer.net/v2.0.min.js"></script>
+
+    <section id="main_section">   <!-- BestOfjQueryPlugins main section -->
+      
+      <div class="relative_left">
+        
+        <h2 id="title">Spice FM 107</h2>
+        
+        <div id="equalizer">
+ <script src="http://scripts.myradiostream.com/s6/12306/status.js"></script>
+
+<script src="http://scripts.myradiostream.com/s6/12306/listeners.js"></script>
+
+        </div>
+        <div class="clear"></div>
+        </div>
+        <div class="clear"></div>
+
+    </section>    <!-- /BestOfjQueryPlugins main section -->
+    
+    <script src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.reverseorder.js"></script>
+    <script type="text/javascript" src="js/jquery.equalizer.js"></script>
+    <script src="http://scripts.myradiostream.com/s6/12306/status.js?online=Live&offline=Off Air&nosource=On Air Soon"></script>
+    <script>
+      $(document).ready(function(){
+        
+        $("#ad_container span").click(function(){
+          $("#ad_container").fadeOut(400);
+        });
+        
+      });
+    </script>
+</p>
+							<h2 class="to-animate hero-animate-2">We'd love to talk with you.</h2>
+						</div>
+					</div>
+				</div>
+			</div>		
+		</div>
+
+		<div id="fh5co-main">
+			<div id="fh5co-contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<form action="#" method="post">
+							<div class="col-md-12">
+								<div class="form-group">
+									<label for="name" class="sr-only">Email</label>
+									<input placeholder="Name" id="name" type="text" class="form-control input-lg">
+								</div>	
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<label for="email" class="sr-only">Email</label>
+									<input placeholder="Email" id="email" type="text" class="form-control input-lg">
+								</div>	
+							</div>
+							
+							<div class="col-md-12">
+								<div class="form-group">
+									<label for="message" class="sr-only">Message</label>
+									<textarea placeholder="Message" id="message" class="form-control input-lg" rows="3"></textarea>
+								</div>	
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<input type="submit" class="btn btn-primary btn-lg " value="Send">
+
+								</div>	
+							</div>
+							
+							
+						</form>	
+						
+					</div>
+					<div class="col-md-4">
+						<h3>Contact Us</h3>
+						<p>Spice FM 107 </p>
+						<p>2 Gathorne Street</p>
+						<p>Bradford, West Yorkshire</p>
+						<p>BD8 0ER</p>
+						<p>
+							<a href="#">spicemedia.com</a>
+						</p>
+					</div>
+
+
+				</div>
+			</div>
+
+			</div>
+			<!-- fh5co-contact -->
+
+	<div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2357.363044375246!2d-1.7825705842952357!3d53.78302574961336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487be6c400b5d6d5%3A0xd4f19ec96c96e2b8!2s2+Gathorne+St%2C+Bradford%2C+West+Yorkshire+BD7!5e0!3m2!1sen!2suk!4v1465208481983" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+<div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3341.115664388291!2d73.78872591510061!3d33.13232577309085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391fea8be4ed1ef1%3A0x23f40b9f3ab55e35!2sKotli+Rd%2C+New+Mirpur+City!5e0!3m2!1sen!2suk!4v1465208394780" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+
+		
+		</div>
+		<!-- END fhtco-main -->
+		<footer role="contentinfo" id="fh5co-footer">
+			<a href="#" class="fh5co-arrow fh5co-gotop footer-box"><i class="ti-angle-up"></i></a>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-sm-12 footer-box">
+						<h3 class="fh5co-footer-heading">Get in touch</h3>
+						<ul class="fh5co-social-icons">
+							
+							<li><a href="#"><i class="ti-google"></i></a></li>
+							<li><a href="#"><i class="ti-twitter-alt"></i></a></li>
+							<li><a href="#"><i class="ti-facebook"></i></a></li>	
+							<li><a href="#"><i class="ti-instagram"></i></a></li>
+							<li><a href="#"><i class="ti-dribbble"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-12 footer-box text-center">
+						<div class="fh5co-copyright">
+						<p>&copy; 2016 Spice Media Group. All Rights Reserved. <br>Frakhanda Shah</p>
+						</div>
+					</div>
+					
+				</div>
+				<!-- END row -->
+				<div class="fh5co-spacer fh5co-spacer-md"></div>
+			</div>
+		</footer>
+			
+			
+			
+		<!-- jQuery -->
+		<script src="js/jquery-1.10.2.min.js"></script>
+		<!-- jQuery Easing -->
+		<script src="js/jquery.easing.1.3.js"></script>
+		<!-- Bootstrap -->
+		<script src="js/bootstrap.js"></script>
+		<!-- Owl carousel -->
+		<script src="js/owl.carousel.min.js"></script>
+		<!-- Magnific Popup -->
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<!-- Superfish -->
+		<script src="js/hoverIntent.js"></script>
+		<script src="js/superfish.js"></script>
+		<!-- Easy Responsive Tabs -->
+		<script src="js/easyResponsiveTabs.js"></script>
+		<!-- FastClick for Mobile/Tablets -->
+		<script src="js/fastclick.js"></script>
+		<!-- Parallax -->
+		<script src="js/jquery.parallax-scroll.min.js"></script>
+		<!-- Waypoints -->
+		<script src="js/jquery.waypoints.min.js"></script>
+
+		<!-- Main JS -->
+		<script src="js/main.js"></script>
+
+	</body>
+</html>
